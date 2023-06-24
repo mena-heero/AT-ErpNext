a68a81c1ccc1b36248b20430f3fa45ef666d215d
HeeroCode <menaheero@gmail.com>
2023-06-24 14:17:32 +0000

    Dev2

--- heero/heero/doctype/influencers/influencers.js
+++ heero/heero/doctype/influencers/influencers.js
@@ -1,39 +1,30 @@
-// Copyright (c) 2023, Heero and contributors
-// For license information, please see license.txt
-// heero/heero/doctype/influencers/influencers.js
+frappe.ui.form.on("Influencers", "refresh", function(frm) {
+    frm.add_custom_button(__('Update Subscriber Count'), function(){
+        // Your code for the "Update Subscriber Count" button
+        frappe.call({
+            method: 'heero.heero.doctype.influencers.influencers.update_subscriber_count',
+            args: {
+                docname: frm.doc.name
+            },
+            callback: function(response) {
+                // Show success or error message
+                frappe.msgprint(response.message);
+                frm.refresh_field('subcs');
+            }
+        });
+    }, __("Actions"));
 
-frappe.ui.form.on('Influencers', {
-    refresh: function(frm) {
-        // Add a click event listener to your custom button
-        frm.add_custom_button(__('Update Subscriber Count'), function() {
-            frappe.call({
-                method: 'heero.heero.doctype.influencers.influencers.update_subscriber_count',
-                args: {
-                    docname: frm.doc.name
-                },
-                callback: function(response) {
-                    // Show success or error message
-                    frappe.msgprint(response.message);
-                    frm.refresh_field('subcs');
-                }
-            });
+    frm.add_custom_button(__('Transfer to Lead'), function(){
+        // Your code for the "Transfer to Lead" button
+        frappe.call({
+            method: 'heero.heero.doctype.influencers.influencers.transfer_to_lead',
+            args: {
+                docname: frm.doc.name
+            },
+            callback: function(response) {
+                // Show success or error message
+                frappe.msgprint(response.message);
+            }
         });
-    }
-});
-frappe.ui.form.on('Influencers', {
-    refresh: function(frm) {
-        // Add a click event listener to your custom button
-        frm.add_custom_button(__('Transfer to Lead'), function() {
-            frappe.call({
-                method: 'heero.heero.doctype.influencers.influencers.transfer_to_lead',
-                args: {
-                    docname: frm.doc.name
-                },
-                callback: function(response) {
-                    // Show success or error message
-                    frappe.msgprint(response.message);
-                }
-            });
-        });
-    }
+    }, __("Actions"));
 });
