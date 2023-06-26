c084bf87f7d603ea139af6a726c3537f7c8eb2f6
HeeroCode <menaheero@gmail.com>
2023-06-26 06:40:22 +0000

    advertiser2lead

--- heero/heero/doctype/advertiser/advertiser.js
+++ heero/heero/doctype/advertiser/advertiser.js
@@ -1,8 +1,16 @@
-// Copyright (c) 2023, Heero and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Advertiser', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on("Advertiser", "refresh", function(frm) {
+	frm.add_custom_button(__('Transfer to Lead'), function(){
+        // Your code for the "Transfer to Lead" button
+        frappe.call({
+            method: 'heero.heero.doctype.advertiser.advertiser.transfer_to_lead',
+            args: {
+                docname: frm.doc.name
+            },
+            callback: function(response) {
+                // Show success or error message
+                frappe.msgprint(response.message);
+            }
+        });
+    }, __("Actions"));
+   
 });
--- heero/heero/doctype/advertiser/advertiser.py
+++ heero/heero/doctype/advertiser/advertiser.py
@@ -2,7 +2,35 @@
 # For license information, please see license.txt
 
 # import frappe
+
 from frappe.model.document import Document
+import frappe
+import requests
+import logging
+import re
 
 class Advertiser(Document):
 	pass
+
+
+
+@frappe.whitelist()
+def transfer_to_lead(docname):
+    advertiser = frappe.get_doc("Advertiser", docname)
+    # Check if a lead with the same first_name already exists
+    existing_lead = frappe.get_all("Lead", filters={"first_name": advertiser.website})
+
+    if existing_lead:
+        frappe.msgprint("Lead with the same first name already exists. Transfer aborted.")
+        return
+    # Create a new Lead document
+    lead = frappe.new_doc("Lead")
+    lead.first_name = advertiser.website
+    lead.website = advertiser.website
+    lead.phone = advertiser.phone
+    lead.email = advertiser.email
+    lead.inserted_by = advertiser.inserted_by
+
+    lead.insert(ignore_permissions=True)
+
+    frappe.msgprint("advertiser transferred to Lead successfully.")
