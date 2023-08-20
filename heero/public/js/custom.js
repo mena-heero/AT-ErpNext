e13c0526ebe225e8fc7b4369dd9d7781a7ceee57
HeeroCode <menaheero@gmail.com>
2023-08-20 06:57:49 +0000

    update pages

--- heero/public/js/custom.js
+++ heero/public/js/custom.js
@@ -1,5 +1,5 @@
 
-  (function ($) {
+(function ($) {
   
   "use strict";
 
@@ -9,24 +9,7 @@
     });
     
     // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-      var el = $(this).attr('href');
-      var elWrapped = $(el);
-      var header_height = $('.navbar').height();
   
-      scrollToDiv(elWrapped,header_height);
-      return false;
-  
-      function scrollToDiv(element,navheight){
-        var offset = element.offset();
-        var offsetTop = offset.top;
-        var totalScroll = offsetTop-navheight;
-  
-        $('body,html').animate({
-        scrollTop: totalScroll
-        }, 300);
-      }
-    });
 
     $(window).on('scroll', function(){
       function isScrollIntoView(elem, index) {
@@ -47,7 +30,29 @@
       var timeline = $('#vertical-scrollable-timeline li');
       Array.from(timeline).forEach(isScrollIntoView);
     });
-  
+    
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items: 2,
+            },
+            767:{
+                items: 3,
+            },
+            1200:{
+                items: 4,
+            }
+        }
+    });
+
+
+
+
   })(window.jQuery);
 
 
